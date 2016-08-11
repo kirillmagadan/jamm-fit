da4b2eb6d6eb0f5f288634057bec4d1184fd8d79
kirillmagadan <kirill@reefstudio.ru>
2016-08-11 18:51:15 +0300

    version1

---
+++ js/main.js
@@ -0,0 +1,37 @@
+$(document).ready(function(){
+    $("#sm_slider").smSlider({
+        transition : 'fader',
+        duration : 500,
+        autoPlay : true,
+        autoArr : true,
+        pagination : true,
+        delay : 5000
+    });
+
+    $("#sm-slider").smSlider({
+        transition : 'fader',
+        duration : 500,
+        autoPlay : true,
+        autoArr : true,
+        pagination : true,
+        delay : 5000
+    });
+
+    $("#advantages_slider").smSlider({
+        transition : 'fader',
+        duration : 500,
+        autoPlay : true,
+        autoArr : true,
+        pagination : true,
+        delay : 5000
+    });
+
+    $(".burger").click(function(){
+        $(".nav_header_wrap nav").slideToggle(300);
+        return false;
+    });
+
+});
+
+
+
